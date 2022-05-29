530b5f009bcc328806ce7b1b4fafefa1504ee266
minhgau31 <52059331+minhgau31@users.noreply.github.com>
2022-05-29 11:21:44 +0700

    Splash Screen1

--- Assets/Kim/Script/SplashScreen.cs
+++ Assets/Kim/Script/SplashScreen.cs
@@ -8,7 +8,7 @@ public class SplashScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SplashScreen();
+       StartCoroutine(Screen());
     }
 
     // Update is called once per frame
@@ -16,7 +16,7 @@ public class SplashScreen : MonoBehaviour
     {
         
     }
-    IEnumerator SplashScreen()
+    IEnumerator Screen()
     {
 
        yield return new WaitForSeconds (9);
